b1770b9bde2dc43da0014b434affa35ab3aea76d
Francis Stokes <stokesie14@gmail.com>
2021-10-26 21:10:06 +0200

    Add slice32 and bit utilities

--- src/util.ts
+++ src/util.ts
@@ -7,3 +7,16 @@ export const untwos = (v: number) => v >> 0;
 export const boolToInt = (x: boolean) => Number(x);
 
 export const signExtend32 = (bits: number, value: number) => (value << (32 - bits)) >> (32 - bits);
+
+export const slice32 = (from: number, to: number, value: number, position: number = 0) => {
+  const span = from - to + 1;
+  const sliced = ((value >> to) & ((1 << span) - 1));
+
+  if (position !== 0) {
+    return sliced << (position - span);
+  }
+
+  return sliced;
+}
+
+export const bit = (index: number, value: number, position: number = 0) => ((value >> index) & 1) << (position-1);
